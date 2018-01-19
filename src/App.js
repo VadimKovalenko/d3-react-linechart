1dc98359c2cb4adf8a7140851a095b791322a1f6
VadimKovalenko <vadblaxmith@gmail.com>
2018-01-19 22:17:55 +0200

    add line chart with plot --partial impl

--- src/App.js
+++ src/App.js
@@ -12,8 +12,9 @@ class App extends Component {
       <h2>d3ia dashboard</h2>
       </div>
       <div>
-      <BarChart data={[5,10,1,3]} size={[500,500]} />
-      <LineChart data={''} size={[500,800]} />
+      {/*<BarChart data={[5,10,1,3]} size={[500,500]} />*/}
+      {/*<LineChart data={''} size={[500,800]} />*/}
+      <PlotLineChart data={''} size={[500,960]} />
       </div>
       </div>
    )
--- src/PlotLineChart.js
+++ src/PlotLineChart.js
@@ -10,16 +10,16 @@ import { scaleTime }  from 'd3-scale'
 import line from 'd3-shape/src/area'
 import extent from 'd3-array/src/extent'
 import { axisBottom, axisLeft } from 'd3-axis'
-import { tsv } from 'd3'
+import { csv } from 'd3'
 import zoom from 'd3-zoom/src/zoom'
 import transform from 'd3-zoom/src/transform'
 
 
-// example - https://bl.ocks.org/deristnochda/1ffe16ccf8bed2035ea5091ab9bb53fb
+// example - http://bl.ocks.org/natemiller/7dec148bb6aab897e561
 
-var chartData = require('./data.tsv');
+var chartData = require('./climate4.csv');
 
-class LineChart extends Component {
+class PlotLineChart extends Component {
    constructor(props){
       super(props)
       this.createLineChart = this.createLineChart.bind(this)
@@ -32,82 +32,133 @@ class LineChart extends Component {
    }
    createLineChart() {
       const node = this.node
-      const margin = {top: 20, right: 20, bottom: 30, left: 50}
-      const width = node.width.baseVal.value - margin.left - margin.right
-      const height = node.height.baseVal.value - margin.top - margin.bottom
+      const margin = {top: 10, right: 10, bottom: 100, left: 40}
+      const margin2 = {top: 430, right: 10, bottom: 20, left: 40}
+      const width = 960 - margin.left - margin.right
+      const height = 500 - margin.top - margin.bottom
+      const height2 = 500 - margin2.top - margin2.bottom
+
+      const color = d3.scaleOrdinal(d3.schemeCategory10);
+
+      const parseTime = timeParse("%Y%m");
+
+      const x = scaleTime().range([0, width]),
+            x2 = scaleTime().range([0, width]),
+            y = scaleLinear().range([height, 0]),
+            y2 = scaleLinear().range([height2, 0]);
+
       const xAxis = axisBottom(x);
+      const xAxis2 = axisBottom(x2);
       const yAxis = axisLeft(y);
-      const parseTime = timeParse("%d-%b-%y");
 
-      const x = scaleTime()
-      .rangeRound([0, width]);
-      const y = scaleLinear()
-      .rangeRound([height, 0]);
+      let chartLine = line()
+                  .defined(function(d) { return !isNaN(d.temperature); })
+                  .curve(d3.curveBasis)
+                  .x(function(d) { return x(d.date); })
+                  .y(function(d) { return y(d.temperature); });
 
-      //Prepare function to parse data
-      const chartLine = line()
-      .x(function(d) { return x(d.date); })
-      .y(function(d) { return y(d.close); });
+      let chartLine2 = line()
+                  .defined(function(d) { return !isNaN(d.temperature); })
+                  .curve(d3.curveBasis)
+                  .x(function(d) { return x2(d.date); })
+                  .y(function(d) { return y2(d.temperature); });
 
-
-   tsv(chartData, function(d) {
-      d.date = parseTime(d.date);
-      d.close = +d.close;
-      return d;
-      }, function(error, data) {
-      if (error) throw error;
-
-      x.domain(extent(data, function(d) { return d.date; }));
-      y.domain(extent(data, function(d) { return d.close; }));   
-
-      const gX = select(node).append("g")
-         .attr("transform", "translate(" + margin.left + "," + margin.top + ")").append("g")
-         .attr("transform", "translate(0," + height + ")")
-         .call(axisBottom(x));
-
-      const gY = select(node).append("g")
-         .attr("transform", "translate(" + margin.left + "," + margin.top + ")").append("g")
-         .attr("class", "axis axis--y")
-         .attr("y", 6)
-         .attr("dy", "0.71em")
-         .attr("text-anchor", "end")
-         .text("Price ($)")
-         .call(axisLeft(y));
-
-      //Zooming functionality
-      const zoomChart = zoom()
-      .scaleExtent([1, 40])
-      .extent([[100, 100], [width-100, height-100]])
-      .on("zoom", zoomed);
-
-      function zoomed() {
-         selectAll(".charts")
-         .attr("transform", d3.event.transform);
-         d3.selectAll('.line').style("stroke-width", 2/d3.event.transform.k);
-         gX.call(xAxis.scale(d3.event.transform.rescaleX(x)));
-         gY.call(yAxis.scale(d3.event.transform.rescaleY(y)));
-      }
-      /*/////////////*/
       const svg = select(node) 
          .attr("width", width + margin.left + margin.right)
-         .attr("height", height + margin.top + margin.bottom)
-         .call(zoomChart)
-         .append("g")
-           .attr("transform", "translate(" + margin.left + "," + margin.top + ")");      
+         .attr("height", height + margin.top + margin.bottom); 
 
+
+   csv(chartData, function(error, data) {
+
+      if (error) throw error;
+
+      let brush = d3.brushX()
+      .on("end", brushed)
 
       // Line chart   
-      svg.append("g")
-         .attr("class", "charts")
-         .append("path")
-         .datum(data)
-         .attr("class", "line")
-         .attr("fill", "none")
-         .attr("stroke-linejoin", "round")
-         .attr("stroke-linecap", "round")
-         .attr("stroke", "steelblue")
-         .attr("stroke-width", 1.5)
-         .attr("d", function(d) { return chartLine(d); });
+      svg.append("defs").append("clipPath")
+         .attr("id", "clip")
+         .append("rect")
+            .attr("width", width)
+            .attr("height", height);
+
+      const focus = svg.append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+            
+      const context = svg.append("g")
+        .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
+
+      color.domain(d3.keys(data[0]).filter(function(key) { return key !== "date"; }));
+       
+      data.forEach(function(d) {
+         d.date = parseTime(d.date);
+      });
+
+      let sources = color.domain().map(function(name) {
+         return {
+            name: name,
+            values: data.map(function(d) {
+               return {date: d.date, temperature: +d[name]};
+            })
+         };
+      });
+
+      x.domain(d3.extent(data, function(d) { return d.date; }));
+      y.domain([d3.min(sources, function(c) { return d3.min(c.values, function(v) { return v.temperature; }); }),
+              d3.max(sources, function(c) { return d3.max(c.values, function(v) { return v.temperature; }); }) ]);
+      x2.domain(x.domain());
+      y2.domain(y.domain());
+
+      let focuslineGroups = focus.selectAll("g")
+                                 .data(sources)
+                                 .enter().append("g");
+
+      let focuslines = focuslineGroups.append("path")
+                                       .attr("class","line")
+                                       .attr("d", function(d) { return chartLine(d.values); })
+                                       .style("stroke", function(d) {return color(d.name);})
+                                       .attr("clip-path", "url(#clip)");
+
+      focus.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
+      focus.append("g")
+            .attr("class", "y axis")
+         .selectAll("rect")
+            .attr("height", 60)
+            .call(yAxis);
+
+      let contextlineGroups = context.selectAll("g")
+                                       .data(sources)
+                                       .enter().append("g");
+
+      let contextLines = contextlineGroups.append("path")
+                                          .attr("class", "line")
+                                          .attr("d", function(d) { return chartLine2(d.values); })
+                                          .style("stroke", function(d) {return color(d.name);})
+                                          .attr("clip-path", "url(#clip)");
+
+      context.append("g")
+               .attr("class", "x axis")
+               .attr("transform", "translate(0," + height2 + ")")
+               .call(xAxis2);
+
+      context.append("g")
+               .attr("class", "x brush") 
+               .call(brush)   
+
+      function brushed() {
+         /*If brush is removed, show all data. Else - show only in selected domain */
+         x.domain(/*select("brush").call(brush.move, null) ? x2.domain() : */d3.event.selection);
+         focus.selectAll("path.line").attr("d",  function(d) {return chartLine(d.values)});
+         focus.select(".x.axis").call(xAxis);
+         focus.select(".y.axis").call(yAxis);
+         //console.log(select(".brush").call(brush.move, null))
+         //console.log(x2.domain())
+      }
+
    })
 }
 
@@ -117,4 +168,4 @@ render() {
       </svg>
    }
 }
-export default LineChart
+export default PlotLineChart
